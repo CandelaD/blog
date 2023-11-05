9908fc3670a42e0dcc33ed001edb090d865b6a3c
Julián Figueroa <juliaanfigueroaa@gmail.com>
2023-11-05 18:15:10 +0100

    fix app

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
+from urllib.parse import urlparse
 from flask import Flask, render_template, request, redirect, url_for
 from forms import SignupForm
 from flask_login import LoginManager
-from models import get_user
+from models import get_user, users
 from flask_login import LoginManager, current_user, login_user, logout_user
-from werkzeug.urls import url_parse
-
 
 app = Flask(__name__)
 
 login_manager = LoginManager(app)
 
 @app.route('/')
-def index():
-    return ('inicio')
+def home():
+    return render_template('signup_form.html', form=SignupForm())
 
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
@@ -38,7 +37,7 @@ def login():
         if user is not None and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
+            if not next_page or urlparse(next_page).netloc != '':
                 next_page = url_for('index')
             return redirect(next_page)
     return render_template('login_form.html', form=form)
@@ -48,10 +47,12 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
 @login_manager.user_loader
 def load_user(user_id):
     for user in users:
         if user.id == int(user_id):
             return user
     return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
